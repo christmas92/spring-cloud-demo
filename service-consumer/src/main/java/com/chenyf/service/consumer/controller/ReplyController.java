da7bacbc9bae9d7f927640c134781d4b51c61085
chenyifei <278805774@qq.com>
2018-06-05 17:45:56 +0800

    add ribbon
    Add Ribbon for Loadbalance
    1.给RestTemplate添加@LoadBalanced注解
    2.配置文件中给服务添加设置负载均衡策略

--- service-consumer/src/main/java/com/chenyf/service/consumer/controller/ReplyController.java
+++ service-consumer/src/main/java/com/chenyf/service/consumer/controller/ReplyController.java
@@ -1,6 +1,8 @@
 package com.chenyf.service.consumer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +15,17 @@ public class ReplyController {
 	@Autowired
 	private RestTemplate restTemplate;	
 	
+	@Autowired
+	private LoadBalancerClient loadBalancerClient;
+	
 	@GetMapping
 	public String reply() {
-		return this.restTemplate.getForObject("http://localhost:9000/hello/say", String.class);
+		return this.restTemplate.getForObject("http://service-provider1/hello/say", String.class);
+	}
+	
+	@GetMapping("loadBalance")
+	public void loadBalance() {
+		ServiceInstance instance = loadBalancerClient.choose("service-provider1");
+		System.out.println(instance.getServiceId() + ":" + instance.getHost() + ":" + instance.getPort());
 	}
 }
